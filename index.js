b08f4b86c9e8e253e87eea06e8d61e3328589b05
Sitoh7 <ha.wakhule@gmail.com>
2024-09-17 12:27:27 +0300

    use array functions

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+    return cats;
+
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat() {
+    const appendCat = [...cats, "Broom"];
+    return appendCat;
+}
+
+function prependCat() {
+    const prependCat = [ "Arnold", ...cats];
+    return prependCat;
+
+}
+
+function removeLastCat() {
+    const removeLastCat = cats.slice(0,2);
+  return (removeLastCat);
+}
+
+
+function removeFirstCat() {
+    const removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+}
